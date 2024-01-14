72d687595c49b3ec420c4baddbad8f35f01a3392
kojikawazu <nanairo7.daydream21@gmail.com>
2024-01-14 22:56:56 +0900

    EC2にIAM追加

---
+++ sample/07-ec2-iam/app_server.tf
@@ -0,0 +1,40 @@
+# ---------------------------------------------
+# key pair
+# ---------------------------------------------
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.project}-${var.environment}-keypair"
+  public_key = file("${var.key_pair_path}")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Project = var.project
+    Env     = var.environment
+  }
+}
+
+# ---------------------------------------------
+# EC2 instance
+# ---------------------------------------------
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id
+  ]
+
+  iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
+  key_name             = aws_key_pair.keypair.key_name
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-ec2"
+    Project = var.project
+    Env     = var.environment
+    Type    = "app"
+  }
+
+  user_data = file("./scripts/startup.sh")
+}
