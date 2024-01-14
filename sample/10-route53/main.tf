05dccd81c8a5a9af9e2a9966134f8cefa98eddaf
kojikawazu <nanairo7.daydream21@gmail.com>
2024-01-14 23:01:06 +0900

    Route53追加

---
+++ sample/10-route53/main.tf
@@ -0,0 +1,43 @@
+# ---------------------------------------------
+# Terraform configuration
+# ---------------------------------------------
+terraform {
+  required_version = ">=1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "key_pair_path" {
+  type = string
+}
+
+variable "ami_name" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
