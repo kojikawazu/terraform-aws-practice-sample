4c83771ce23f27f3a666331d123b2473099c23a7
kojikawazu <nanairo7.daydream21@gmail.com>
2024-01-25 08:02:15 +0900

    12-cloudfront-elb追加

---
+++ sample/12-cloudfront-elb/main.tf
@@ -0,0 +1,73 @@
+# ---------------------------------------------
+# Terraform configuration
+# ---------------------------------------------
+terraform {
+  required_version = ">=1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+provider "aws" {
+  alias   = "virginia"
+  profile = "terraform"
+  region  = "us-east-1"
+}
+
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "project" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_address" {
+  type = string
+}
+
+variable "public_1a_address" {
+  type = string
+}
+
+variable "public_1c_address" {
+  type = string
+}
+
+variable "private_1a_address" {
+  type = string
+}
+
+variable "private_1c_address" {
+  type = string
+}
+
+variable "key_pair_path" {
+  type = string
+}
+
+variable "ec2_instance_type" {
+  type = string
+}
+
+variable "ami_name" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
