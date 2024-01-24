fc600c29beb475861d9b2229bcb4fd7791296cc9
kojikawazu <nanairo7.daydream21@gmail.com>
2024-01-25 08:20:29 +0900

    99-master修正

--- sample/99-master/network.tf
+++ sample/99-master/network.tf
@@ -2,7 +2,7 @@
 # VPC
 # ---------------------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_block                       = "192.168.0.0/20"
+  cidr_block                       = var.vpc_address
   instance_tenancy                 = "default"
   enable_dns_support               = true
   enable_dns_hostnames             = true
@@ -22,7 +22,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet_1a" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
-  cidr_block              = "192.168.11.0/24"
+  cidr_block              = var.public_1a_address
   map_public_ip_on_launch = true
 
   tags = {
@@ -33,10 +33,11 @@ resource "aws_subnet" "public_subnet_1a" {
   }
 }
 
+# public 1c
 resource "aws_subnet" "public_subnet_1c" {
   vpc_id                  = aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1c"
-  cidr_block              = "192.168.12.0/24"
+  cidr_block              = var.public_1c_address
   map_public_ip_on_launch = true
 
   tags = {
@@ -44,6 +45,36 @@ resource "aws_subnet" "public_subnet_1c" {
     Project = var.project
     Env     = var.environment
     Type    = "public"
+  }
+}
+
+# private 1a
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = var.private_1a_address
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1a"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+# private 1c
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = var.private_1c_address
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1c"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
   }
 }
 
@@ -69,6 +100,27 @@ resource "aws_route_table_association" "public_rt_1a" {
 resource "aws_route_table_association" "public_rt_1c" {
   route_table_id = aws_route_table.public_rt.id
   subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt"
+    Project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private_rt_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
 }
 
 # ---------------------------------------------
