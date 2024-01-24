6d08c610d4f7d8dc38949a3d9e78aadabc0557c6
kojikawazu <nanairo7.daydream21@gmail.com>
2024-01-25 07:54:22 +0900

    08-ec2-ami修正

--- sample/08-ec2-ami/main.tf
+++ sample/08-ec2-ami/main.tf
@@ -30,7 +30,31 @@ variable "environment" {
   type = string
 }
 
+variable "vpc_address" {
+  type = string
+}
+
+variable "public_1a_address" {
+  type = string
+}
+
+variable "public_1c_address" {
+  type = string
+}
+
+variable "private_1a_address" {
+  type = string
+}
+
+variable "private_1c_address" {
+  type = string
+}
+
 variable "key_pair_path" {
+  type = string
+}
+
+variable "ec2_instance_type" {
   type = string
 }
 
